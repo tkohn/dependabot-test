9270ed9e7eba79547da1fdfeab9156d8d5d60920
Torsten Kohn <torsten@Torstens-Air-2.fritz.box>
2024-07-25 08:24:18 +0200

    Generate test project with start.spring.io

---
+++ src/main/java/dev/kohn/dependabottest/DependabotTestApplication.java
@@ -0,0 +1,13 @@
+package dev.kohn.dependabottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependabotTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependabotTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/kohn/dependabottest/DependabotTestApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kohn.dependabottest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependabotTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
